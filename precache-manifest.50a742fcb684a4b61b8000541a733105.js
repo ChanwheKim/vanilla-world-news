b3fa59d9e889365f2f1c366d8290f47bfdf48e0f
ChanwheKim <chanwhe.kim@gmail.com>
2019-04-05 16:20:40 +0900

    Updates

---
+++ precache-manifest.50a742fcb684a4b61b8000541a733105.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c57856dac8bf2cc112c5",
+    "url": "/vanilla-world-news/static/js/runtime~main.c57856da.js"
+  },
+  {
+    "revision": "b03b6eaa9d4d649ee8f5",
+    "url": "/vanilla-world-news/static/js/main.b03b6eaa.chunk.js"
+  },
+  {
+    "revision": "28702b17b0b14f1431f5",
+    "url": "/vanilla-world-news/static/js/1.28702b17.chunk.js"
+  },
+  {
+    "revision": "b03b6eaa9d4d649ee8f5",
+    "url": "/vanilla-world-news/static/css/main.f948a59b.chunk.css"
+  },
+  {
+    "revision": "2dfe95cbf2588dc7aef5e783d5885eb5",
+    "url": "/vanilla-world-news/index.html"
+  }
+];
